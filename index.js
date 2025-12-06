aa9e23ecf5ced1206f623b3423daac84c7c5e27a
kondrashhhh <hurstay4@gmail.com>
2025-12-06 03:43:00 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+require('dotenv').config()
+const PORT = process.env.PORT
+
+app.get("/stars", (req, res) => {
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is started on port: ${PORT}...`)
+})
